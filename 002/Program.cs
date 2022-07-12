55f33fb537ecbfdf3b5ea6dc0bc11e2ed69783fe
Savenkova Mariya <sm3989@yandex.ru>
2022-07-12 18:30:10 +0300

    add 002 excercise

---
+++ 002/Program.cs
@@ -0,0 +1,25 @@
+﻿// по двум заданным числам проверить является ли первое квадратом второго
+
+double a,b;
+string s=Console.ReadLine();
+a=Convert.ToDouble(s);
+s=Console.ReadLine();
+b=Convert.ToDouble(s);
+
+if (a*a==b)
+{
+    System.Console.WriteLine("b is square a");
+}
+else
+{
+    System.Console.WriteLine("b isn`t square a");
+}
+
+if (b*b==a)
+{
+    System.Console.WriteLine("a is square b");
+}
+else
+{
+    System.Console.WriteLine("a isn`t square b");
+}
