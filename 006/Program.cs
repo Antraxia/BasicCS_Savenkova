4cbecb612cec61bf49852e26094069e177523112
Savenkova Mariya <sm3989@yandex.ru>
2022-07-14 13:19:43 +0300

    add 006 exercise

---
+++ 006/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу вычисления значения функции y=sin(a).
+
+double a,y;
+
+System.Console.WriteLine("Введите а, для вычисления значения функции y=sin(a): ");
+string s=Console.ReadLine();
+a=Convert.ToDouble(s);
+
+y=Math.Sin(a);
+
+System.Console.WriteLine($"у = {y}");
