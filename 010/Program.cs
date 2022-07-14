4b4683d54da0b974020c45b7feea3eae2251c85b
Savenkova Mariya <sm3989@yandex.ru>
2022-07-14 18:22:28 +0300

    add 008, 010, 011, 012, 013, 014, 015

---
+++ 010/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вывести на экран последнюю цифру числа введенного с клавиатуры
+
+int a,b;
+System.Console.WriteLine("Введите число: ");
+string s=Console.ReadLine();
+a=Convert.ToInt32(s);
+
+b=a%10;
+
+System.Console.WriteLine($"Последняя цифра числа {b}");
+
+
---
+++ 011/Program.cs
@@ -0,0 +1,11 @@
+﻿// Дано число больше 9. Вывести на экран вторую цифру числа с конца
+
+int a,b,c;
+System.Console.WriteLine("Введите число: ");
+string s=Console.ReadLine();
+a=Convert.ToInt32(s);
+
+b=a/10;
+c=b%10;
+
+System.Console.WriteLine($"Вторая цифра с конца: {c}");
