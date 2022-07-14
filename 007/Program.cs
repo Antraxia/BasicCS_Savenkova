a5ce65c28e18b7b274189ec8b740a45b4b2f2e86
Savenkova Mariya <sm3989@yandex.ru>
2022-07-14 13:26:16 +0300

    add 007 exercise

---
+++ 007/Program.cs
@@ -0,0 +1,16 @@
+﻿// Выяснить является ли число четным
+
+int a;
+
+System.Console.WriteLine("Введите число: ");
+string s=Console.ReadLine();
+a=Convert.ToInt32(s);
+
+if (a%2==0)
+{
+    System.Console.WriteLine($"{a} четное");
+}
+else
+{
+    System.Console.WriteLine($"{a} не четное");
+}
