90b77c02b3af1f6e6e88ab0c7b1cf422ed14be0f
Savenkova Mariya <sm3989@yandex.ru>
2022-07-18 02:04:42 +0300

    add 021

---
+++ 021/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+System.Console.WriteLine("Введите номер четверти: ");
+int a=Convert.ToInt32(Console.ReadLine());
+
+if (a==1)
+{
+    System.Console.WriteLine("x>0 ; y>0");
+}
+else if (a==2)
+{
+    System.Console.WriteLine("x<0 ; y>0");
+}
+else if (a==3)
+{
+    System.Console.WriteLine("x>0 ; y<0");
+}
+else
+{
+    System.Console.WriteLine("x<0 ; y<0");
+}
