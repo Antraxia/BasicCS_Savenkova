156bf8bb763b37fe1e0893dd7938d27ff26e5f84
Savenkova Mariya <sm3989@yandex.ru>
2022-08-06 00:36:47 +0300

    add 041

---
+++ 041/Program.cs
@@ -0,0 +1,34 @@
+﻿//В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+
+int N=12;
+int[] a=new int[N];
+
+Random random=new Random();
+for (int i=0;i<a.Length;i++)
+{
+     a[i]=random.Next(1,100);
+
+}
+int max=a[0];
+int min=a[0];
+int diff=0;
+
+for(int i=0;i<a.Length;i++)
+{
+
+     if (a[i]>max)
+     {
+         max=a[i];
+     }
+     else if (a[i]<min)
+     {
+         min=a[i];
+     }
+
+
+     System.Console.Write($"{a[i]} ");
+}
+
+diff=max-min;
+System.Console.WriteLine();
+System.Console.WriteLine($"минимальный элемент = {min}, максимальный элемент = {max}, разница между ними = {diff}");
