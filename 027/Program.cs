aa10d55df76017e6cc3424255b0d9b93c847194c
Savenkova Mariya <sm3989@yandex.ru>
2022-08-05 22:49:15 +0300

    add 027

---
+++ 027/Program.cs
@@ -0,0 +1,15 @@
+﻿// Возведите число А в натуральную степень В используя цикл
+
+System.Console.WriteLine("Введите число: ");
+int a=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите степень: ");
+int b=Convert.ToInt32(Console.ReadLine());
+
+int c=1;
+for (int i=1; i<=b;i++)
+{
+    c=c*a;
+
+}
+
+System.Console.WriteLine($"{c} ");
