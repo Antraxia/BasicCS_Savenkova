2239c672d820693652248c3f26d4ba432674412d
Savenkova Mariya <sm3989@yandex.ru>
2022-08-05 22:39:46 +0300

    add 026

---
+++ 026/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти сумму чисел от 1 до A
+
+System.Console.WriteLine("Введите число: ");
+int a=Convert.ToInt32(Console.ReadLine());
+int sum=0;
+
+for (int i=1;i<a;i++)
+{
+    sum=sum+a;
+}
+
+System.Console.Write($"{sum} ");
