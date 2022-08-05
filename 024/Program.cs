e023f571d7d82d14ae95507bf7c48e3f707e1877
Savenkova Mariya <sm3989@yandex.ru>
2022-08-05 22:30:21 +0300

    add 024

---
+++ 024/Program.cs
@@ -0,0 +1,15 @@
+﻿// Вывести на экран таблицу квадратов чисел от 1 до N
+
+System.Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+for (int i=1;i<=N;i++)
+{
+       double a=Math.Pow(i, 2);
+       System.Console.Write($"{a} ");
+}
+
+
