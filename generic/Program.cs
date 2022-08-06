2786cd17e93576bc050d65464c07808347018e36
Savenkova Mariya <sm3989@yandex.ru>
2022-08-06 21:45:15 +0300

    change generic file

--- generic/Program.cs
+++ generic/Program.cs
@@ -1,7 +1,9 @@
 ﻿// Generic - обобщения, шаблоны
 
+
 class Program   // чтобы несколько методов с одним названием работали для разных типов данных их нужно поместить в класс
 {
+    /*
     static void Swap(ref int a,ref int b)
     {
         int t=a;
@@ -34,5 +36,41 @@ class Program   // чтобы несколько методов с одним н
        Swap(ref e,ref t);
        Init(out a);
  
+    }
+    */
+
+
+
+
+public static void Swap<T>(ref T a, ref T b)  // <T> параметризорованный тип T-type, тип данных будет задаваться при вызове Swap
+{
+    T t=a;
+    a=b;
+    b=t;
+
+}
+    static void Init(out int[] a)
+    {
+        a=new int[10];
+        for(int i=0;i<10;i++)
+        {
+            a[i]=10;
+        }
+    }
+ 
+}
+
+class MyClass
+{
+    static void main()
+    {
+        int[] a;
+        int x=1,y=2;
+        double e=2,t=4;
+
+        Program.Swap(ref x,ref y);
+        Program.Swap(ref e,ref t);
+        Program.Init(out a);
+        Program.Swap<int>(ref x,ref y); // <int> указывает тип данных
     }
 }
