63dc77a6b53eb8c31dfa332efdcfb2b14618ee16
Savenkova Mariya <sm3989@yandex.ru>
2022-08-07 15:51:27 +0300

    change 033

--- 033/Program.cs
+++ 033/Program.cs
@@ -1,11 +1,43 @@
 ﻿// Задать массив из 8 элементов, заполненных нулями и единицами их на экран
 
-int N=8;
-int[] a=new int[N];
-Random random=new Random();
-for (int i=0;i<a.Length;i++)
 
-    a[i]=random.Next(0,2);
+int[] a;
+Init (out a,8,0,1);
+Print(a);
 
-    for(int i=0;i<a.Length;i++)
-    System.Console.WriteLine($"{a[i]} ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    void Init(out int[] a,int Length,int min, int max)  
+{
+     a=new int[Length];
+     Random random=new Random(); 
+
+for (int i=0;i<a.Length;i++) 
+     a[i]=random.Next(min,max+1);
+}
+
+
+void Print(int[] a)
+{
+    for (int i=0;i<a.Length;i++)
+    System.Console.WriteLine($"{a[i],4}");  
+}
