862a76d5b1b1955d1a723ec37130b6d86a42b5e7
Savenkova Mariya <sm3989@yandex.ru>
2022-08-07 16:41:57 +0300

    change 037

--- 037/Program.cs
+++ 037/Program.cs
@@ -1,23 +1,53 @@
 ﻿// Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
 
-int N=8;
-int c=0;
-int b=0;
-int[] a=new int[N];
-Random random=new Random();
-for (int i=0;i<a.Length;i++)
-{
-    a[i]=random.Next(100,999);
 
-    if(a[i]%2!=0)
+
+int[] a;
+int b,c;
+Init(out a,8,99,999);
+CountNumbers(a,out b,out c);
+Print(a);
+
+
+
+
+
+
+
+
+
+
+
+
+    void Init(out int[] a,int Length,int min, int max)  
+{
+     a=new int[Length];
+     Random random=new Random(); 
+
+for (int i=0;i<a.Length;i++) 
+     a[i]=random.Next(min,max+1);
+}
+
+void CountNumbers(int[] a, out int b,out int c)
+{
+    b=0;
+    c=0;
+    for (int i=0;i<a.Length;i++)
     {
-        b++;
-    }
-    else
-    {
-        c++;
+        if(a[i]%2!=0)
+        {
+           b++;
+        }
+        else
+        {
+           c++;
+        }
     }
 }
-for (int i=0;i<a.Length;i++)
-System.Console.WriteLine($"{a[i]} ");
-System.Console.WriteLine($"четных чисел - {c}, нечетных чисел - {b}");
+
+void Print(int[] a) 
+{
+    for (int i=0;i<a.Length;i++)
+    System.Console.WriteLine($"{a[i]} ");
+    System.Console.WriteLine($"четных чисел - {c}, нечетных чисел - {b}");
+}
