8f943b6a7e9c041753570eecb919eb9d61b98492
Savenkova Mariya <sm3989@yandex.ru>
2022-08-07 23:30:05 +0300

    change 038

--- 038/Program.cs
+++ 038/Program.cs
@@ -7,50 +7,24 @@ Init(out a,123,0,150);
 Segment(a,out k);
 Print(a);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    void Init(out int[] a,int Length,int min, int max)  
+void Init(out int[] a,int Length,int min, int max)  
 {
      a=new int[Length];
      Random random=new Random(); 
-
      for (int i=0;i<a.Length;i++) 
      a[i]=random.Next(min,max+1);
 }
 
-
-
 void Segment(int[] a,out int k)
-{
-    
+{    
     k=0;
     for (int i=0;i<a.Length;i++) 
     {
         if (a[i]>10 && a[i]<99)
      {
          k++;
-    
+     }
     }
-    else
-    {
-        k=0;
-    }
-    }
-   
 }  
 
 void Print(int[] a) 
