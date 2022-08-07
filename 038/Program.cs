e891c248d1dd59c62091e2381044fe780cd62433
Savenkova Mariya <sm3989@yandex.ru>
2022-08-07 17:12:11 +0300

    change 038

--- 038/Program.cs
+++ 038/Program.cs
@@ -1,23 +1,59 @@
 ﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
 
-int N=123;
-int[] a=new int[N];
-int k=0;
-Random random=new Random();
-for (int i=0;i<a.Length;i++)
+
+int[] a;
+int k;
+Init(out a,123,0,150);
+Segment(a,out k);
+Print(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    void Init(out int[] a,int Length,int min, int max)  
 {
-     a[i]=random.Next(0,123);
+     a=new int[Length];
+     Random random=new Random(); 
 
-     if (a[i]>10 && a[i]<99)
-     {
-         k++;
-     }
-     else
-     {
-         k=0;
-     }
-
+     for (int i=0;i<a.Length;i++) 
+     a[i]=random.Next(min,max+1);
 }
 
-System.Console.Write($"{k}");
-System.Console.WriteLine();
+
+
+void Segment(int[] a,out int k)
+{
+    
+    k=0;
+    for (int i=0;i<a.Length;i++) 
+    {
+        if (a[i]>10 && a[i]<99)
+     {
+         k++;
+    
+    }
+    else
+    {
+        k=0;
+    }
+    }
+   
+}  
+
+void Print(int[] a) 
+{
+    System.Console.Write($"{k} ");
+}
