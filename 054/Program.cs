efb204c8e1c5ee035235efb93683c7c65b706601
Savenkova Mariya <sm3989@yandex.ru>
2022-08-13 02:28:16 +0300

    add 054

---
+++ 054/Program.cs
@@ -0,0 +1,20 @@
+﻿//Написать подпрограмму, которая в матрице чисел находит сумму элементов главной диагонали
+
+int[,] arr;
+arr=Create2DArray(3,3);
+Print(arr);
+
+int sumMain(int[,] arr)
+{
+int sum=0;
+for(int i=0;i<arr.GetLength(0);i++)
+    sum=sum+arr[i,i];
+    return sum;
+}
+    System.Console.WriteLine($"сумма элементов главной диагонали - {sumMain(arr)}");
+
+int sum2=0;
+for(int i=0;i<arr.GetLength(0);i++)
+    sum2=sum2+arr[i,(arr.GetLength(0)-i-1)];
+    System.Console.WriteLine($"сумма элементов побочной диагонали - {sum2}");
+    
