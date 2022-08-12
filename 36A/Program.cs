d4dddebc1e7475e63395ad3fb309ee43bd8a366a
Savenkova Mariya <sm3989@yandex.ru>
2022-08-12 23:05:40 +0300

    add 36A

---
+++ 36A/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] a;
+int sum;
+Init(out a,8,0,15);
+CountNumbers(a,out sum);
+Print(a);
+
+
+    void Init(out int[] a,int Length,int min, int max)  
+{
+     a=new int[Length];
+     Random random=new Random(); 
+
+for (int i=0;i<a.Length;i++) 
+     a[i]=random.Next(min,max);
+}
+
+void CountNumbers(int[] a, out int sum)
+{
+    sum=0;
+    for (int i=0;i<a.Length;i++)
+    {
+        if(i%2!=0)
+        {
+           sum=sum+a[i];
+        }
+    }
+}
+
+void Print(int[] a) 
+{
+    for (int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i]} ");
+    System.Console.WriteLine();
+System.Console.WriteLine($"{sum}");
+}
