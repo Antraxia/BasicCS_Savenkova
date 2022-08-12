125a154592bbd3374d64fdd8fc3517c3fd55a21d
Savenkova Mariya <sm3989@yandex.ru>
2022-08-12 22:49:09 +0300

    add 048

---
+++ 048/Program.cs
@@ -0,0 +1,24 @@
+﻿// Написать программу копирования массива
+int N=12;
+int[] a=new int[N];
+int[] b=new int[N];
+
+Random random=new Random();
+for (int i=0;i<a.Length;i++)
+   {
+     a[i]=random.Next(1,10);
+   }
+
+for (int i=0;i<a.Length;i++)
+{
+    b[i]=a[i];
+} 
+
+
+for (int i=0;i<a.Length;i++)
+System.Console.Write($"{a[i]} ");
+System.Console.WriteLine();
+for (int i=0;i<a.Length;i++)
+System.Console.Write($"{b[i]} ");
+
+
