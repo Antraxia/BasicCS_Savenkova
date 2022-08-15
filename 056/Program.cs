7f87d9b8d478a3af92b3396e789aa85e4620df06
Savenkova Mariya <sm3989@yandex.ru>
2022-08-16 00:36:39 +0300

    add 056

---
+++ 056/Program.cs
@@ -0,0 +1,44 @@
+﻿// Написать программу, которая обменивает элементы первой строки и последней строки
+
+int[,] arr; 
+arr=Create2DArray(3,4);
+Print (arr);
+Solve (arr,0);
+System.Console.WriteLine();
+Print (arr);
+
+
+int[,] Create2DArray (int n,int m)
+{
+    int[,] a=new int[n,m];
+    Random random=new Random();
+    for (int i=0;i<a.GetLength(0);i++) 
+        for(int j=0;j<a.GetLength(1);j++)
+            a[i,j]=random.Next(0,100);
+    return a;
+}
+
+
+void Solve (int[,] a, int change)
+{
+         for (int i=0;i<a.GetLength(0);i++) 
+               for(int j=0;j<a.GetLength(1);j++)
+               {
+                change=a[0,j];
+                   a[0,j]=a[a.GetLength(0)-1,j];
+                   a[a.GetLength(0)-1,j]=change; 
+               }
+                                      
+}   
+
+void Print(int[,] a)
+{
+    for(int i=0;i<a.GetLength(0);i++)
+    {
+        for(int j=0;j<a.GetLength(1);j++)
+        System.Console.Write($"{a[i,j],4} ");
+        System.Console.WriteLine();
+        
+    }
+   
+}
