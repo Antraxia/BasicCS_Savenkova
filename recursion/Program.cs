4cee15c108f89543d9ea6743a1e393895a52c97e
Savenkova Mariya <sm3989@yandex.ru>
2022-08-17 22:53:55 +0300

    add recursion

---
+++ recursion/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу вывода чисмел от 1 до N без использования циклов и оператора goto
+
+void Loop(int i,int N)
+{
+    System.Console.WriteLine(i);
+    if (i<N) Loop(i+1,N);
+}
+
+Loop (1,10);
