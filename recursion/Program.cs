bfa562570b4c8272f6d8a3aed2da20a5fdd5f59e
Savenkova Mariya <sm3989@yandex.ru>
2022-08-17 23:43:39 +0300

    add 069

--- recursion/Program.cs
+++ recursion/Program.cs
@@ -7,3 +7,20 @@ void Loop(int i,int N)
 }
 
 Loop (1,10);
+
+int F(int n)
+{
+    if (n>0) return F(n-1)*n;
+    else
+       return 1;
+}
+ System.Console.WriteLine(F(5));
+
+ int Sum(int i,int N)
+ {
+    if (i<=N) return Sum(i+1,N)+i;
+    else
+       return 0;
+ }
+
+ System.Console.WriteLine(Sum(0,3));
