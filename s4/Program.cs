2e58892ffe49bbff878c8ad5a43863723f2a8db5
Savenkova Mariya <sm3989@yandex.ru>
2022-08-20 23:24:22 +0300

    add s4

---
+++ s4/Program.cs
@@ -0,0 +1,8 @@
+﻿// Подсчитать количество цифр среди вводимых с клавиатуры символов
+string s=Console.ReadLine();
+int sum=0;
+for(int i=0;i<s.Length;i++)
+    //if (s[i]>='0' && s[i]<='9')
+    if (char.IsDigit(s[i]))
+        sum=sum+1;//(int)(char.GetNumericValue(s[i])); //Преобразуем s[i] в число с помощью GetNumericValue, но она приобретает тип double, чтобы преобразовать его в тип int, используется данный способ
+System.Console.WriteLine(sum);        
