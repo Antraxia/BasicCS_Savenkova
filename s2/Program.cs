0062a8f19a8440839045606f067334f8b033a5c0
Savenkova Mariya <sm3989@yandex.ru>
2022-08-20 23:14:32 +0300

    add s2

---
+++ s2/Program.cs
@@ -0,0 +1,20 @@
+﻿// 1.Вывести на экран кодировку символов по ASCII(с 32 до 122)
+
+string s;
+for(int i=32;i<=122;i++)
+{
+    System.Console.Write((char)i);
+}
+
+//2.Вывести на экран символы от A до Z как показано ниже:
+//A
+//BB
+//CCC
+
+for(int i=65;i<=90;i++)
+{
+    for(int j=0;j<i-64;j++)
+        System.Console.Write((char)i);
+
+    System.Console.WriteLine();
+}
