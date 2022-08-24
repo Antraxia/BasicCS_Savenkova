3b30493b5fce5d07dfb332dd6e7b999bd40b83e3
Savenkova Mariya <sm3989@yandex.ru>
2022-08-25 01:51:52 +0300

    add events

---
+++ events/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Zayarniy
+{
+    delegate void Flag(); // сигнатура void()
+    class A 
+    {
+        public void IsFlagTrue()
+        {
+            System.Console.WriteLine("Flag is true");
+        }
+
+    }
+
+    class B 
+    {
+        private int flage=0;
+        public Flag FLAG;
+
+        public void Work()
+        {
+            Random random=new Random();
+            for(int i=0;i<2000;i++)
+               {
+                if (random.Next(1,1000)==500)
+                {
+                    flage=1;
+                    FLAG();
+                }
+               }
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            A a=new A();
+            B b=new B();
+            b.FLAG=new Flag(a.IsFlagTrue);
+            b.Work();
+        }
+    }
+}
