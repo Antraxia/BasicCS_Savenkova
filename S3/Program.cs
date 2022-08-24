80099fee0548ca3aade9ba090716003f1cefc0f6
Savenkova Mariya <sm3989@yandex.ru>
2022-08-24 22:30:29 +0300

    add s3

---
+++ S3/Program.cs
@@ -0,0 +1,9 @@
+﻿//С клавиатуры вводится строка разделенная точкой. Подсчитать количество символов до точки
+
+string str = "qwerty.qwe";
+int countBefore = str.IndexOf('.');
+int count=0;
+char[] cc=str.ToArray();
+for(int i=0;i<countBefore;i++)
+    count++;
+System.Console.WriteLine(count);
